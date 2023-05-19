6eadc41e6f75344f6336037404b321e10665b80c
dong wook kim <ehddnr73@naver.com>
2023-05-19 22:24:11 +0900

    Battery 관련 EventListener 등록

--- hello/www/js/EventListenerManager.js
+++ hello/www/js/EventListenerManager.js
@@ -5,13 +5,36 @@ class EventListenerManager {
 
   register() {
     document.addEventListener("deviceready", this.#onDeviceReady(), false);
-    document.addEventListener("click", this.#clickGeolocationButton());
+    window.addEventListener("batterystatus", this.#onBatteryStatus, false);
+    window.addEventListener("batterylow", this.#onBatteryLow, false);
+    window.addEventListener("batterycritical", this.#onBatteryCritical, false);
+    this.#registerButtonClickEvent();
   }
 
+  #registerButtonClickEvent() {
+    document.addEventListener("click", this.#clickGeolocationButton());
+    document.addEventListener("click", this.#clickBatteryStatusButton());
+  }
+
+  // Device ready event
   #onDeviceReady() {
 
   }
 
+  // Battery event
+  #onBatteryStatus(status) {
+    console.log("Level: " + status.level + " isPlugged: " + status.isPluged);
+  }
+
+  #onBatteryLow(status) {
+    alert("Battery Level Low " + status.level + "%");
+  }
+
+  #onBatteryCritical(status) {
+    alert("Battery Level Critical " + status.level + "%\nRecharge Soon!");
+  }
+
+  // Geolocation event
   #clickGeolocationButton() {
     const geolocationService = new GeolocationService();
     geolocationService.getPosition();
