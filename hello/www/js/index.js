2c9e30ae654bcf8a463540f3f985c886827e36b7
dong wook kim <ehddnr73@naver.com>
2023-05-22 17:06:33 +0900

    Dialogs 관련 함수 추가

--- hello/www/js/index.js
+++ hello/www/js/index.js
@@ -1,4 +1,57 @@
-import EventListenerManager from "./EventListenerManager.js";
+// Functions of Dialogs
+function clickAlertButton() {
+  navigator.notification.alert("Hello World!", null, "Alert", "Done");
+}
 
-const eventListenerManager = new EventListenerManager();
-eventListenerManager.register();
+function clickConfirmButton() {
+  navigator.notification.confirm(
+    "You are the winner!", 
+    onConfirm, 
+    "Game Over", 
+    ["Restart", "Exit"]
+  );
+}
+
+function onConfirm(buttonIndex) {
+  alert("You selected button " + buttonIndex);
+}
+
+function clickPromptButton() {
+  navigator.notification.prompt(
+    "Please enter your name", 
+    onPrompt, 
+    "Registration", 
+    ["OK", "Exit"], 
+    "Yeolmok"
+  );
+}
+
+function onPrompt(results) {
+  alert("You selected button number " + results.buttonIndex + " and entered " + results.input1);
+}
+
+function clickBeepButton() {
+  navigator.notification.beep(2);
+}
+
+// Device Ready
+document.addEventListener('deviceready', function() {
+  onDeviceReady()
+}, false);
+
+function onDeviceReady() {
+    
+}
+
+// Event register of Dialogs
+const alertButton = document.getElementById("alert");
+alertButton.addEventListener("click", clickAlertButton);
+
+const confirmButton = document.getElementById("confirm");
+confirmButton.addEventListener("click", clickConfirmButton);
+
+const promptButton = document.getElementById("prompt");
+promptButton.addEventListener("click", clickPromptButton);
+
+const beepButton = document.getElementById("beep");
+beepButton.addEventListener("click", clickBeepButton);
