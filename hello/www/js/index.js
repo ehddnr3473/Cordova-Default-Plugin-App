5f682c87fb32c2fcbf83ebf03ea96d793d2e4fec
dong wook kim <ehddnr73@naver.com>
2023-05-23 08:20:17 +0900

    Device 관련 함수 작성

--- hello/www/js/index.js
+++ hello/www/js/index.js
@@ -60,6 +60,39 @@ function onBatteryCritical(status) {
   navigator.notification.alert("Battery Level Critical " + status.level + "%\nRecharge Soon!");
 }
 
+// Fuction of Device
+function clickCordovaButton() {
+  navigator.notification.alert(device.cordova);
+}
+
+function clickPlatformButton() {
+  navigator.notification.alert(device.platform);
+}
+
+function clickModelButton() {
+  navigator.notification.alert(device.model);
+}
+
+function clickUUIDButton() {
+  navigator.notification.alert(device.uuid);
+}
+
+function clickVersionButton() {
+  navigator.notification.alert(device.version);
+}
+
+function clickManufacturerButton() {
+  navigator.notification.alert(device.manufacturer);
+}
+
+function clickIsVirtualButton() {
+  navigator.notification.alert(device.isVirtual);
+}
+
+function clickSerialButton() {
+  navigator.notification.alert(device.serial);
+}
+
 // Device Ready
 function onDeviceReady() {
   // Event register of Dialogs
@@ -83,7 +116,25 @@ function onDeviceReady() {
   window.addEventListener("batterystatus", onBatteryStatus, false);
   window.addEventListener("batterylow", onBatteryLow, false);
   window.addEventListener("batterycritical", onBatteryCritical, false);
+
+  // Event register of Device
+  const cordovaButton = document.getElementById("device-cordova");
+  const modelButton = document.getElementById("device-model");
+  const platformButton = document.getElementById("device-platform");
+  const uuidButton = document.getElementById("device-uuid");
+  const versionButton = document.getElementById("device-version");
+  const manufacturerButton = document.getElementById("device-manufacturer");
+  const isVirtualButton = document.getElementById("device-is-virtual");
+  const serialButton = document.getElementById("device-serial");
+
+  cordovaButton.addEventListener("click", clickCordovaButton);
+  modelButton.addEventListener("click", clickModelButton);
+  platformButton.addEventListener("click", clickPlatformButton);
+  uuidButton.addEventListener("click", clickUUIDButton);
+  versionButton.addEventListener("click", clickVersionButton);
+  manufacturerButton.addEventListener("click", clickManufacturerButton);
+  isVirtualButton.addEventListener("click", clickIsVirtualButton);
+  serialButton.addEventListener("click", clickSerialButton);
 }
 
 document.addEventListener('deviceready', onDeviceReady, false);
-
